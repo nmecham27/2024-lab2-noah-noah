8c282d46c8aa8e867806fdce77493c42ef8e701c
Noah Lomu <noah.lomu@gmail.com>
2024-09-21 00:45:03 -0600

    Import updates from the template

--- test/hello_world.robot
+++ test/hello_world.robot
@@ -2,4 +2,4 @@
 hello_world on rpi_pico
     ${x}=                       Execute Command         include @${CURDIR}/test/simulate.resc
     Create Terminal Tester      sysbus.uart0    timeout=15    defaultPauseEmulation=true
-    Wait For Line On Uart       Hello World!
+    Wait For Line On Uart       hello world!
--- test/test.c
+++ test/test.c
@@ -1,5 +1,8 @@
+#include <stdio.h>
+#include <pico/stdlib.h>
 #include <stdint.h>
 #include <unity.h>
+#include "unity_config.h"
 
 void setUp(void) {}
 
@@ -13,16 +16,20 @@ void test_variable_assignment()
 
 void test_multiplication(void)
 {
-    int x = 5;
+    int x = 30;
     int y = 6;
-    int z = x * y;
-    TEST_ASSERT_TRUE_MESSAGE(z == 30, "Multiplication of two integers returned incorrect value.");
+    int z = x / y;
+    TEST_ASSERT_TRUE_MESSAGE(z == 5, "Multiplication of two integers returned incorrect value.");
 }
 
 int main (void)
 {
+    stdio_init_all();
+    sleep_ms(5000); // Give time for TTY to attach.
+    printf("Start tests\n");
     UNITY_BEGIN();
     RUN_TEST(test_variable_assignment);
     RUN_TEST(test_multiplication);
+    sleep_ms(5000);
     return UNITY_END();
 }
